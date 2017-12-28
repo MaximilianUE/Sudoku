6659bd3b065f9cacbf699cbf9633f9cef69ec21b
MaximilianUE <maximilian.becher@ueberbit.de>
2017-12-29 00:10:22 +0100

    add test values

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+
+/**
+ A   B   C
+ --- --- ---
+ D   E   F
+ --- --- ---
+ G   H   I
+**/
+var gridA = [1,2,3,4,5,6,7,8,9];
+var gridB = [1,2,3,4,5,6,7,8,9];
+var gridC = [1,2,3,4,5,6,7,8,9];
+var gridD = [1,2,3,4,5,6,7,8,9];
+var gridE = [1,2,3,4,5,6,7,8,9];
+var gridF = [1,2,3,4,5,6,7,8,9];
+var gridG = [1,2,3,4,5,6,7,8,9];
+var gridH = [1,2,3,4,5,6,7,8,9];
+var gridI = [1,2,3,4,5,6,7,8,9];
+
+var gridBox = [gridA, gridB, gridC, gridD, gridE, gridF, gridG, gridH, gridI];
+
 function testMe() {
 
     arr = [];
