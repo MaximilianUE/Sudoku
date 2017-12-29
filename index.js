de1d5886b5bdb2965c7e81fd1708627949458ca8
MaximilianUE <maximilian.becher@ueberbit.de>
2017-12-29 01:32:04 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 
-/**
- A   B   C
- --- --- ---
- D   E   F
- --- --- ---
- G   H   I
-**/
-var gridA = [1,2,3,4,5,6,7,8,9];
-var gridB = [1,2,3,4,5,6,7,8,9];
-var gridC = [1,2,3,4,5,6,7,8,9];
-var gridD = [1,2,3,4,5,6,7,8,9];
-var gridE = [1,2,3,4,5,6,7,8,9];
-var gridF = [1,2,3,4,5,6,7,8,9];
-var gridG = [1,2,3,4,5,6,7,8,9];
-var gridH = [1,2,3,4,5,6,7,8,9];
-var gridI = [1,2,3,4,5,6,7,8,9];
-
-var gridBox = [gridA, gridB, gridC, gridD, gridE, gridF, gridG, gridH, gridI];
-
 function main() {
 
     givenValues = [
@@ -25,20 +6,22 @@ function main() {
         [],[],[],
         [],[],[]
     ];
-    possibleValues = [];
-    //getAllColumnValues();
+    
+    possibleValues = [
+        [],[],[],
+        [],[],[],
+        [],[],[]
+    ];
+
     getAllColumnValues();
+
     console.log('givenValues:');
     console.log(givenValues);
 }
 
 function getAllColumnValues() {
     const row = document.querySelectorAll('.row');
-    /*
-    row.forEach(function(row) {
-        getOneColumnValue(row);
-    });
-    */
+
     for(i = 0; i < row.length; i++) {
         getOneColumnValue(row[i])
     }
@@ -54,6 +37,8 @@ function getOneColumnValue(row) {
     theseGivenValues.sort();
     givenValues[i].push(theseGivenValues);
 }
+
+
 
 function getOneColumnPossibleValues() {
 
