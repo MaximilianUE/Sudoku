0fe3624743ffa974b7af6b3841e1cf61e7a1a0b3
MaximilianUE <maximilian.becher@ueberbit.de>
2017-12-31 01:04:52 +0100

    split js to focus index.js on main function

--- index.js
+++ index.js
@@ -1,59 +1,22 @@
 
 function main() {
 
-    columns_GivenValues = [
-        [],[],[],
-        [],[],[],
-        [],[],[]
-    ];
+    column_GetAllValues();
 
-    columns_PossibleValues = [
-        [],[],[],
-        [],[],[],
-        [],[],[]
-    ];
-
-    columns_GetAllValues();
+    /*
+    rows_GetAllValues();
     console.log("-----------------------");
-    console.log("result");
-    console.log(columns_PossibleValues);
+    console.log("result rows");
+    console.log(rows_PossibleValues);
     console.log("-----------------------");
-}
-
-function columns_GetAllValues() {
-    const row = document.querySelectorAll('.row');
-
-    for(i = 0; i < row.length; i++) {
-        columns_GetOneGivenValue(row[i]);
-        columns_GetOnePossibleValue();
-    }
-}
-
-function columns_GetOneGivenValue(row) {
-    const column = row.querySelectorAll('.column');
-    var column_GivenValues = [];
-
-    column.forEach(function(e) {
-        column_GivenValues.push(+e.innerHTML.trim());
-    });
-    column_GivenValues.sort();
-    return columns_GivenValues[i].push(column_GivenValues);
-}
 
 
-function columns_GetOnePossibleValue() {
-    var column_PossibleValues = [1,2,3,4,5,6,7,8,9];
+    column_GetAllValues();
+    console.log("-----------------------");
+    console.log("result columns");
+    console.log(columns_GivenValues);
+    console.log("-----------------------");
 
-    for (t = 1; t <= 9; t++) {
+    */
 
-        if (columns_GivenValues[i][0].includes(t)) {
-
-            const remove = column_PossibleValues.indexOf(t);
-            if (remove !== -1) {
-                column_PossibleValues.splice(remove, 1);
-            }
-        }
-    }
-    column_PossibleValues.sort();
-    return columns_PossibleValues[i].push(column_PossibleValues);
 }
