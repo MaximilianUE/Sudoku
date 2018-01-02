0818e1f9ba57455133dbb0114d2179c849ab602c
MaximilianUE <maximilian.becher@ueberbit.de>
2018-01-02 02:16:24 +0100

    backup code

--- index.js
+++ index.js
@@ -75,4 +75,74 @@ function field_getAllPossibleValues(field) {
     if(field_PossibleValues.length === 1) {
         field.innerText = field_PossibleValues;
     }
+
+    /*
+    console.log(grid_PossibleValues);
+
+    //loop through grid_GivenValues
+    for (var g = 0; g < grid_PossibleValues.length; g++) {
+
+      field_PossibleValues.push(+grid_PossibleValues[g])
+
+    }
+
+    console.log("-----------------------");
+    console.log("result:");
+    console.log(field_PossibleValues);
+
+
+    console.log("row");
+    console.log(row_PossibleValues);
+    //loop through row_GivenValues
+    for (g = 0; g < row_PossibleValues.length; g++) {
+
+        field_PossibleValues.push(+row_PossibleValues[g]);
+        field_PossibleValues.push(+8);
+
+    }
+
+    console.log("-----------------------");
+    console.log("result:");
+    console.log(field_PossibleValues);
+
+*/
+
+
+
+
+    /*
+    for (var r = 0; r <= row_PossibleValues; r++) {
+
+        const check = field_PossibleValues.indexOf(r);
+        if (check === -1) {
+            field_PossibleValues.push(row_PossibleValues[r])
+        }
+
+    }
+
+    for (var c = 0; c <= column_PossibleValues; c++) {
+
+        const check = field_PossibleValues.indexOf(c);
+        if (check === -1) {
+            field_PossibleValues.push(column_PossibleValues[c])
+        }
+
+
+
+    }
+
+        /*
+
+        if ( (grid_PossibleValues.includes(t) && row_PossibleValues.includes(t)) ||
+             (grid_PossibleValues.includes(t) && column_PossibleValues.includes(t)) ||
+             (row_PossibleValues.includes(t) && column_PossibleValues.includes(t)) ) {
+            const remove = field_PossibleValues.indexOf(t);
+            console.log(remove);
+            if (remove !== -1) {
+                field_PossibleValues.splice(remove, 1);
+            }
+        }
+    }
+    */
 }
+
